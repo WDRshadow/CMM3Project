250f909ff50c1070885643ab9b67237f4e992f81
Jonathan Hsu <i@wdr.im>
2021-11-04 11:37:21 +0000

    Update main.py, a big change

--- demos/Zekai_demo.py
+++ demos/Zekai_demo.py
@@ -1,47 +1,222 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from random import gauss
 import matplotlib as mpl
 from matplotlib.colors import ListedColormap
-import math
-# x_coord1 = [np.random.uniform(0,100) for i in range(10)]
-Np = 65536
-Xmin = Ymin = -1
-Xmax = Ymax = 1
-Particle_b = math.ceil(math.pi * (0.3**2)/1 * Np)
-Particle_r = Np - Particle_b
-x_pr = []
-y_pr = []
-for i in range(Particle_r):
-    x = np.random.uniform(Xmin,Xmax)
-    y = np.random.uniform(Xmin,Xmax)
-    if (x**2 + y**2) >= 0.3**2:
-        x_pr.append(x)
-        y_pr.append(y)
+import seaborn as sns
 
-x_pb = []
-y_pb = []
 
-for i in range(Particle_b):
-    x = np.random.uniform(-0.3,0.3)
-    y = np.random.uniform(-0.3,0.3)
-    if (x**2 + y**2)<0.3**2:
-        x_pb.append(x)
-        y_pb.append(y)
+# boundary condition
+def BC(x, y):
+    if x < -1:
+        x = -2 - x
+    else:
+        if x > 1:
+            x = 2 - x
+    if y < -1:
+        y = -2 - y
+    else:
+        if y > 1:
+            y = 2 - y
+    return x, y
 
-cmap = ListedColormap(["red", "blue"])
-norm = norm = mpl.colors.Normalize(vmin=0, vmax=1)
-fig=plt.figure(figsize=(10,8))
-ax2 = fig.add_subplot(111)
-ax2.scatter(x_pb,y_pb,s = 1,c = 'b')
-ax2.scatter(x_pr,y_pr,s = 1,c = 'r')
-ax2.set_title("2D problem")
-ax2.set_xlabel("x")
-ax2.set_ylabel("y")
-plt.xlim([-1,1])
-plt.ylim([-1,1])
-ax2.set_xticks([-1,-0.5,0,0.5,1])
-ax2.set_yticks([-1,-0.5,0,0.5,1])
-im3=plt.scatter(0,0,c=0,cmap=cmap, norm=norm)
-fig.colorbar(im3)
-plt.show()
+
+# for easier setting, I put all the code into a class, developed by York and The Kite
+class CMM3(object):
+    def __init__(self):
+        # ----------------------------------------
+        # init conditions / interface
+        # time set, "h" is a step time
+        self.time_max = 0.4
+        self.h = 0.05
+        # diffusivity
+        self.D = 0.01
+        # domain size
+        self.x_min = -1
+        self.x_max = 1
+        self.y_min = -1
+        self.y_max = 1
+        # number of axial gridlines and total grids
+        self.Nx = 64
+        self.Ny = 64
+        self.grids = self.Nx * self.Ny
+        # number of particles
+        self.Np = 65536
+        # init velocity
+        self.vel = 0
+        # -----------------------------------------
+        # for temp particle position data save
+        self.x_blue = []
+        self.y_blue = []
+        self.x_red = []
+        self.y_red = []
+        # figure and save the interval of the gridlines
+        self.ivl_grid_y = (self.y_max - self.y_min) / self.Ny
+        self.ivl_grid_x = (self.x_max - self.x_min) / self.Nx
+        # data0 is set to count and save the number of red particles and blue particles
+        self.data0 = np.zeros((self.Nx, self.Ny, 2))
+        # data is set to figure the proportion of blue particles in each grid
+        self.data = np.zeros((self.Nx, self.Ny))
+
+    # the func below is extends the classical Euler method
+    def Euler_method(self, Xp):
+        # create a gauss random dx
+        r = gauss(0, 1)
+        # equation 6
+        # 'r' is random numbers with the standard Gaussian probability
+        # 'vel' is the (known) velocity components evaluated at the particle position
+        Xp += self.vel * self.h + math.sqrt(2 * self.D) * math.sqrt(self.h) * r
+        return Xp
+
+    # this func is used to update the particles after a step time, the movement of each particle is built by York,
+    def go_a_step(self):
+        for i in range(len(self.x_blue)):
+            self.x_blue[i] = self.Euler_method(self.x_blue[i])
+            self.y_blue[i] = self.Euler_method(self.y_blue[i])
+            # use the boundary condition above
+            self.x_blue[i], self.y_blue[i] = BC(self.x_blue[i], self.y_blue[i])
+        for i in range(len(self.x_red)):
+            self.x_red[i] = self.Euler_method(self.x_red[i])
+            self.y_red[i] = self.Euler_method(self.y_red[i])
+            # use the boundary condition above
+            self.x_red[i], self.y_red[i] = BC(self.x_red[i], self.y_red[i])
+
+    # this func is used to setup the particles in one traverse, built by York
+    def setup(self):
+        # to reset the coordinates and quantities for the circulation in main()
+        self.x_blue = []
+        self.y_blue = []
+        self.x_red = []
+        self.y_red = []
+        # use np.random to init the particles
+        for i in range(self.Np):
+            # temp val for a random particle
+            tx = np.random.uniform(self.x_min, self.x_max)
+            ty = np.random.uniform(self.y_min, self.y_max)
+            ivl_xs = math.ceil((tx - self.x_min) / self.ivl_grid_x) - 1
+            ivl_ys = math.ceil((ty - self.y_min) / self.ivl_grid_y) - 1
+            # select the color of this particle and count each color in data0
+            if math.sqrt(math.pow(tx, 2) + math.pow(ty, 2)) < 0.3:
+                self.x_blue.append(tx)
+                self.y_blue.append(ty)
+                self.data0[ivl_xs][ivl_ys][0] += 1
+            else:
+                self.x_red.append(tx)
+                self.y_red.append(ty)
+                self.data0[ivl_xs][ivl_ys][1] += 1
+
+    def grid(self):
+        # to locate which grid is the new red particle in and add it to data0[1]
+        for i in range(len(self.x_blue)):
+            ivl_xs = math.ceil((self.x_blue[i] - self.x_min) / self.ivl_grid_x) - 1
+            ivl_ys = math.ceil((self.y_blue[i] - self.y_min) / self.ivl_grid_y) - 1
+            self.data0[ivl_xs][ivl_ys][0] += 1
+        # to locate which grid is the new red particle in and add it to data0[1]
+        for i in range(len(self.x_red)):
+            ivl_xs = math.ceil((self.x_red[i] - self.x_min) / self.ivl_grid_x) - 1
+            ivl_ys = math.ceil((self.y_red[i] - self.y_min) / self.ivl_grid_y) - 1
+            self.data0[ivl_xs][ivl_ys][1] += 1
+        # transfer the data0 into data by calculating the proportion of blue particles in each grid
+        for i in range(self.Nx):
+            for j in range(self.Ny):
+                self.data[i][j] = self.data0[i][j][0] / (self.data0[i][j][0] + self.data0[i][j][1])
+
+    # show1 and show2 are made by The Kite
+    # the visualization of particle form
+    def show1(self):
+        # set the figure and pass in the coordinates of blue and red particles
+        plt.figure(figsize=(10, 8))
+        plt.scatter(self.x_blue, self.y_blue, s=1, c='b')
+        plt.scatter(self.x_red, self.y_red, s=1, c='r')
+        # set the layout of axis and title
+        plt.title("2D problem", fontname='Arial', fontsize=30, weight='bold')
+        plt.xlabel("x", fontname='Arial', fontsize=20, weight='bold')
+        plt.ylabel("y", fontname='Arial', fontsize=20, weight='bold')
+        plt.xlim([-1, 1])
+        plt.ylim([-1, 1])
+        plt.xticks([-1 + i * 0.5 for i in range(5)])
+        plt.yticks([-1 + i * 0.5 for i in range(5)])
+        # set the parameters of the colorbar in particle form and create it
+        cmap = ListedColormap(["red", "blue"])
+        norm = mpl.colors.Normalize(vmin=0, vmax=1)
+        plt.colorbar(
+            mpl.cm.ScalarMappable(cmap=cmap, norm=norm)
+        )
+        # show
+        plt.show()
+
+    # the visualization of grid form
+    def show2(self, colors):
+        # pass in data and create the heatmap
+        sns_plot = sns.heatmap(self.data, vmin=0, vmax=1, cmap=colors)
+        # set the layout of axis and title
+        plt.title("2D problem", fontname='Arial', fontsize=30, weight='bold')
+        sns_plot.set_xlabel("x", fontname='Arial', fontsize=20, weight='bold')
+        sns_plot.set_ylabel("y", fontname='Arial', fontsize=20, weight='bold')
+        plt.axis('off')
+        # show
+        plt.show()
+
+    '''
+    # This code is built by York, and used to test
+    def show(self):
+        plt.scatter(self.x_red, self.y_red, s=0.5, color='red')
+        plt.scatter(self.x_blue, self.y_blue, s=0.5, color='blue')
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.show()
+    '''
+
+    # main code for this class
+    def main(self):
+        # the defination of colorbar of gird form
+        colors1 = [(r, g, b) for (r, g, b) in zip(np.linspace(1, 0.8, 7), np.linspace(0, 0, 7), np.linspace(0, 0.9, 7))]
+        colors2 = [(r, g, b) for (r, g, b) in zip(np.linspace(0.7, 0, 6), np.linspace(0, 1, 6), np.linspace(1, 0, 6))]
+        colors3 = [(r, g, b) for (r, g, b) in zip(np.linspace(0, 0, 7), np.linspace(0.7, 0, 7), np.linspace(0, 1, 7))]
+        colors = colors1 + colors2 + colors3
+
+        print("This is CMM3 group's project of 2D probelms")
+        while True:
+            choice = int(input("for particle press 0,for grid press 1,press anything to quit\n"))
+            # the visualization of particle form
+            if choice == 0:
+                # setup the init list of particles
+                self.setup()
+                # show the first graph when t = 0
+                self.show1()
+                # cycle in Classic Euler Method step by step
+                for i in range(int(self.time_max / self.h)):
+                    self.data = np.zeros((self.Nx, self.Ny))
+                    self.go_a_step()
+                    self.show1()
+                # after the iteration recreate data0 and data
+                self.data0 = np.zeros((self.Nx, self.Ny, 2))
+                self.data = np.zeros((self.Nx, self.Ny))
+            # the visualization of grid form
+            elif choice == 1:
+                # setup the init list of particles, data0 and data
+                self.setup()
+                # show the first graph when t = 0, data0 and data
+                self.grid()
+                self.show2(colors)
+                # cycle in Classic Euler Method step by step
+                for i in range(int(self.time_max / self.h)):
+                    self.data = np.zeros((self.Nx, self.Ny))
+                    self.go_a_step()
+                    self.grid()
+                    self.show2(colors)
+                # after the iteration recreate data0 and data
+                self.data0 = np.zeros((self.Nx, self.Ny, 2))
+                self.data = np.zeros((self.Nx, self.Ny))
+            # quit the project
+            else:
+                return
+
+
+# main code for the whole project
+if __name__ == '__main__':
+    # build a instance
+    Project = CMM3()
+    # to run main code
+    Project.main()
--- main.py
+++ main.py
@@ -7,33 +7,14 @@ from matplotlib.colors import ListedColormap
 import seaborn as sns
 from sys import exit
 
-# the definition of colorbar of gird form
-colors1 = [(r, g, b) for (r, g, b) in zip(np.linspace(1, 0.8, 7), np.linspace(0, 0, 7), np.linspace(0, 0.9, 7))]
-colors2 = [(r, g, b) for (r, g, b) in zip(np.linspace(0.7, 0, 6), np.linspace(0, 1, 6), np.linspace(1, 0, 6))]
-colors3 = [(r, g, b) for (r, g, b) in zip(np.linspace(0, 0, 7), np.linspace(0.7, 0, 7), np.linspace(0, 1, 7))]
-colors = colors1 + colors2 + colors3
 
-
-# boundary condition
-def BC(x, y):
-    if x < -1:
-        x = -2 - x
-    elif x > 1:
-        x = 2 - x
-    if y < -1:
-        y = -2 - y
-    elif y > 1:
-        y = 2 - y
-    return x, y
-
-
-# for easier setting, I put all the code into a class, developed by York and The Kite
-class CMM3(object):
+# for easier setting, I put all the code into a class
+class TaskA(object):
     def __init__(self):
         # ----------------------------------------
         # init conditions / interface
         # time set, "h" is a step time
-        self.time_max = 1
+        self.time_max = 0.4
         self.h = 0.05
         # diffusivity
         self.D = 0.01
@@ -52,19 +33,10 @@ class CMM3(object):
         self.vel = 0
         # -----------------------------------------
         # for temp particle position data save
-        self.x_blue = []
-        self.y_blue = []
-        self.x_red = []
-        self.y_red = []
-        # figure and save the interval of the gridlines
-        self.ivl_grid_y = (self.y_max - self.y_min) / self.Ny
-        self.ivl_grid_x = (self.x_max - self.x_min) / self.Nx
-        # data0 is set to count and save the number of red particles and blue particles
-        self.data0 = np.zeros((self.Nx, self.Ny, 2))
-        # data is set to figure the proportion of blue particles in each grid
-        self.data = np.zeros((self.Nx, self.Ny))
+        self.x_data = [[], []]
+        self.y_data = [[], []]
 
-    # the func below is extends the classical Euler method
+    # the func below is the classical Euler method
     def Euler_method(self, Xp):
         # create a gauss random dx
         r = gauss(0, 1)
@@ -74,66 +46,50 @@ class CMM3(object):
         Xp += self.vel * self.h + math.sqrt(2 * self.D) * math.sqrt(self.h) * r
         return Xp
 
+    # boundary condition
+    def BC(self, x, y):
+        if x < -1:
+            x = -2 - x
+        else:
+            if x > 1:
+                x = 2 - x
+        if y < -1:
+            y = -2 - y
+        else:
+            if y > 1:
+                y = 2 - y
+        return x, y
+
     # this func is used to update the particles after a step time, the movement of each particle is built by York,
-    # the operation to data0 is built by TheKite
     def go_a_step(self):
-        for i in range(len(self.x_blue)):
-            self.x_blue[i] = self.Euler_method(self.x_blue[i])
-            self.y_blue[i] = self.Euler_method(self.y_blue[i])
-            # use the boundary condition above
-            self.x_blue[i], self.y_blue[i] = BC(self.x_blue[i], self.y_blue[i])
-            # to locate which grid is the new blue particle in and add it to data0[0]
-            ivl_xs = math.ceil((self.x_blue[i] - self.x_min) / self.ivl_grid_x) - 1
-            ivl_ys = math.ceil((self.y_blue[i] - self.y_min) / self.ivl_grid_y) - 1
-            self.data0[ivl_xs][ivl_ys][0] += 1
-        for i in range(len(self.x_red)):
-            self.x_red[i] = self.Euler_method(self.x_red[i])
-            self.y_red[i] = self.Euler_method(self.y_red[i])
-            # use the boundary condition above
-            self.x_red[i], self.y_red[i] = BC(self.x_red[i], self.y_red[i])
-            # to locate which grid is the new red particle in and add it to data0[1]
-            ivl_xs = math.ceil((self.x_red[i] - self.x_min) / self.ivl_grid_x) - 1
-            ivl_ys = math.ceil((self.y_red[i] - self.y_min) / self.ivl_grid_y) - 1
-            self.data0[ivl_xs][ivl_ys][1] += 1
-        # transfer the data0 into data by calculating the proportion of blue particles in each grid
-        for i in range(self.Nx):
-            for j in range(self.Ny):
-                self.data[i][j] = self.data0[i][j][0] / (self.data0[i][j][0] + self.data0[i][j][1])
+        for i in range(len(self.x_data)):
+            for n in range(len(self.x_data[i])):
+                self.x_data[i][n] = self.Euler_method(self.x_data[i][n])
+                self.y_data[i][n] = self.Euler_method(self.y_data[i][n])
+                # use the boundary condition above
+                self.x_data[i][n], self.y_data[i][n] = self.BC(self.x_data[i][n], self.y_data[i][n])
 
     # this func is used to setup the particles in one traverse, built by York
     def setup(self):
-        # to reset the coordinates and quantities for the circulation in main()
-        self.x_blue = []
-        self.y_blue = []
-        self.x_red = []
-        self.y_red = []
         # use np.random to init the particles
         for i in range(self.Np):
             # temp val for a random particle
             tx = np.random.uniform(self.x_min, self.x_max)
             ty = np.random.uniform(self.y_min, self.y_max)
-            ivl_xs = math.ceil((tx - self.x_min) / self.ivl_grid_x) - 1
-            ivl_ys = math.ceil((ty - self.y_min) / self.ivl_grid_y) - 1
             # select the color of this particle and count each color in data0
             if math.sqrt(math.pow(tx, 2) + math.pow(ty, 2)) < 0.3:
-                self.x_blue.append(tx)
-                self.y_blue.append(ty)
-                self.data0[ivl_xs][ivl_ys][0] += 1
+                self.x_data[1].append(tx)
+                self.y_data[1].append(ty)
             else:
-                self.x_red.append(tx)
-                self.y_red.append(ty)
-                self.data0[ivl_xs][ivl_ys][1] += 1
-        # transfer the data0 into data by calculating the proportion of blue particles in each grid
-        for i in range(self.Nx):
-            for j in range(self.Ny):
-                self.data[i][j] = self.data0[i][j][0] / (self.data0[i][j][0] + self.data0[i][j][1])
+                self.x_data[0].append(tx)
+                self.y_data[0].append(ty)
 
-    # this func is the visualization built by The Kite
-    def show1(self):
+    # the visualization of particle form, by The Kite
+    def show_particle_form(self):
         # set the figure and pass in the coordinates of blue and red particles
         plt.figure(figsize=(10, 8))
-        plt.scatter(self.x_blue, self.y_blue, s=1, c='b')
-        plt.scatter(self.x_red, self.y_red, s=1, c='r')
+        plt.scatter(self.x_data[1], self.y_data[1], s=1, c='b')
+        plt.scatter(self.x_data[0], self.y_data[0], s=1, c='r')
         # set the layout of axis and title
         plt.title("2D problem", fontname='Arial', fontsize=30, weight='bold')
         plt.xlabel("x", fontname='Arial', fontsize=20, weight='bold')
@@ -151,16 +107,34 @@ class CMM3(object):
         # show
         plt.show()
 
-    # the visualization of grid form
-    # set the parameters of the colorbar in grid form
-    colors1 = [(r, g, b) for (r, g, b) in zip(np.linspace(1, 0.5, 7), np.linspace(0, 0, 7), np.linspace(0, 0.9, 7))]
-    colors2 = [(r, g, b) for (r, g, b) in zip(np.linspace(0.7, 0, 6), np.linspace(0, 1, 6), np.linspace(1, 0, 6))]
-    colors3 = [(r, g, b) for (r, g, b) in zip(np.linspace(0, 0, 7), np.linspace(0.7, 0, 7), np.linspace(0, 1, 7))]
-    colors = colors1 + colors2 + colors3
-
-    def show2(self):
+    # the visualization of grid form, by The Kite
+    def show_grid(self):
+        # generate some initial conditions and data saver
+        # figure and save the interval of the gridlines
+        ivl_grid_y = (self.y_max - self.y_min) / self.Ny
+        ivl_grid_x = (self.x_max - self.x_min) / self.Nx
+        # data0 is set to count and save the number of red particles and blue particles
+        data0 = np.zeros((self.Nx, self.Ny, 2))
+        # data is set to figure the proportion of blue particles in each grid
+        data = np.zeros((self.Nx, self.Ny))
+        # calculate the data0 of grid
+        for i in range(len(self.x_data)):
+            # to locate which grid is the new particle in and add it to data0[i]
+            for n in range(len(self.x_data[i])):
+                ivl_xs = math.ceil((self.x_data[i][n] - self.x_min) / ivl_grid_x) - 1
+                ivl_ys = math.ceil((self.y_data[i][n] - self.y_min) / ivl_grid_y) - 1
+                data0[ivl_xs][ivl_ys][i] += 1
+        # transfer the data0 into data by calculating the proportion of blue particles in each grid
+        for i in range(self.Nx):
+            for j in range(self.Ny):
+                data[i][j] = data0[i][j][1] / (data0[i][j][0] + data0[i][j][1])
+        # the defination of colorbar of gird form
+        colors1 = [(r, g, b) for (r, g, b) in zip(np.linspace(1, 0.8, 7), np.linspace(0, 0, 7), np.linspace(0, 0.9, 7))]
+        colors2 = [(r, g, b) for (r, g, b) in zip(np.linspace(0.7, 0, 6), np.linspace(0, 1, 6), np.linspace(1, 0, 6))]
+        colors3 = [(r, g, b) for (r, g, b) in zip(np.linspace(0, 0, 7), np.linspace(0.7, 0, 7), np.linspace(0, 1, 7))]
+        colors = colors1 + colors2 + colors3
         # pass in data and create the heatmap
-        sns_plot = sns.heatmap(self.data, vmin=0, vmax=1, cmap=colors)
+        sns_plot = sns.heatmap(data, vmin=0, vmax=1, cmap=colors)
         # set the layout of axis and title
         plt.title("2D problem", fontname='Arial', fontsize=30, weight='bold')
         sns_plot.set_xlabel("x", fontname='Arial', fontsize=20, weight='bold')
@@ -172,8 +146,8 @@ class CMM3(object):
     '''
     # This code is built by York, and used to test
     def show(self):
-        plt.scatter(self.x_red, self.y_red, s=0.5, color='red')
-        plt.scatter(self.x_blue, self.y_blue, s=0.5, color='blue')
+        plt.scatter(self.x_data[0], self.y_data[0], s=0.5, color='red')
+        plt.scatter(self.x_data[1], self.y_data[1], s=0.5, color='blue')
         plt.xlabel("x")
         plt.ylabel("y")
         plt.show()
@@ -182,47 +156,37 @@ class CMM3(object):
     # main code for this class
     def main(self):
         print("This is CMM3 group's project of 2D probelms")
-        while True:
-            choice = input("For particle enter 0, for grid enter 1, and if you would like to quit, enter anything else.\n")
-            try:
-                choice = int(choice)
-            except:
-                print("Thanks for using our code.\nQuitting")
-                exit()
-            # the visualization of particle form
-            if choice == 0:
-                # setup the init list of particles
-                self.setup()
-                # show the first graph when t = 0
-                self.show1()
-                # cycle in Classic Euler Method step by step
-                for i in range(int(self.time_max / self.h)):
-                    self.data = np.zeros((self.Nx, self.Ny))
-                    self.go_a_step()
-                    self.show1()
-                # after the iteration recreate data0 and data
-                self.data0 = np.zeros((self.Nx, self.Ny, 2))
-                self.data = np.zeros((self.Nx, self.Ny))
-            # the visualization of grid form
-            elif choice == 1:
-                # setup the init list of particles, data0 and data
-                self.setup()
-                # show the first graph when t = 0, data0 and data
-                self.show2()
-                # cycle in Classic Euler Method step by step
-                for i in range(int(self.time_max / self.h)):
-                    self.data = np.zeros((self.Nx, self.Ny))
-                    self.go_a_step()
-                    self.show2()
-                # after the iteration recreate data0 and data
-                self.data0 = np.zeros((self.Nx, self.Ny, 2))
-                self.data = np.zeros((self.Nx, self.Ny))
-            # quit the project
+        # setup the init list of particles
+        self.setup()
+        choice = input("For particle enter 0, for grid enter 1, and if you would like to quit, enter anything else.\n")
+        try:
+            choice = int(choice)
+        except:
+            print("Thanks for using our code.\nQuitting")
+            exit()
+        # the visualization of particle form
+        if choice == 0:
+            # show the first graph when t = 0
+            self.show_particle_form()
+            # cycle in Classic Euler Method step by step
+            for i in range(int(self.time_max / self.h)):
+                self.go_a_step()
+                self.show_particle_form()
+        # the visualization of grid form
+        elif choice == 1:
+            # show the first graph when t = 0, data0 and data
+            self.show_grid()
+            # cycle in Classic Euler Method step by step
+            for i in range(int(self.time_max / self.h)):
+                self.go_a_step()
+                self.show_grid()
+        print("Thanks for using our code.\nQuitting")
+        exit()
 
 
-# main code for the whole project
+# main code for the whole TaskA
 if __name__ == '__main__':
     # build a instance
-    Project = CMM3()
+    run = TaskA()
     # to run main code
-    Project.main()
+    run.main()
